1aa9e23a5ffa99b3a36b99bf67362c8c1fc9df85
ibruceuk78 <34508865+ibruceuk78@users.noreply.github.com>
2021-11-17 22:54:49 +0000

    Update settings.py

--- myOAuth/settings.py
+++ myOAuth/settings.py
@@ -79,17 +79,19 @@ WSGI_APPLICATION = 'myOAuth.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 
 
 # Password validation
